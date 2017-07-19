6085accad220b8c0792f913d85e2d18aeadf58a0
dailingh <1040126995@qq.com>
2017-07-19 23:58:47 +0800

    second

---
+++ microblog/data/data.js
@@ -0,0 +1,3 @@
+var user = [];
+user[{name: dl,password: 111,},{name: gdp,password: 222}];
+module.export = user
--- microblog/routes/signup.js
+++ microblog/routes/signup.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var crypto = require('crypto');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('signup', { title: 'Sign Up' });
-  next();
+	res.render('signup', { title: 'Sign Up' });
+	next();
 });
-router.post('/',function(req,res){
-	var md5 = crypto.createHash('md5');
-	var password = md5.update(req.body.password).digest('base64');
+router.post('/',function(req, res, next) {
+	var newUser = {
+		name: req.body.username,
+		password: req.body.password,
+	}
+	fs.readFile('../data/data.js', 'utf8', function(err,data){
+		if (err) {
+			console.log(err)
+		}
+		
+	});
 })
 
 module.exports = router;
