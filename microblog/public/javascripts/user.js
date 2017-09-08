2c6b0bd2dbb71eac34957d490787e36d3acccdbf
dailingh <1040126995@qq.com>
2017-09-08 16:23:20 +0800

    repost

--- microblog/public/javascripts/user.js
+++ microblog/public/javascripts/user.js
@@ -1,4 +1,7 @@
-var logout = document.querySelector("button");
+var logout = document.querySelector(".logout");
+var text = document.querySelector(".text");
+var submit = document.querySelector(".submit");
+var content = document.querySelector(".content");
 console.log(logout);
 // logout.addEventListener("click",function () {
 //   axios.post("/logout",{
@@ -10,4 +13,11 @@ console.log(logout);
 // })
 logout.addEventListener("click",function () {
   window.location.href = "/"
+})
+
+submit.addEventListener("click",function () {
+  var tpl = document.createElement("div");
+  tpl.className = "div";
+  tpl.innerText = text;
+  content.appendChild("tpl");
 })
